7293f96940326b889d88d16e5319192abcf812ce
Alex Gleason <alex@alexgleason.me>
2016-05-01 15:55:05 -0500

    Add cache to queried data

--- dxe_chapters_map/server.py
+++ dxe_chapters_map/server.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, Response
+from werkzeug.contrib.cache import SimpleCache
 from dxe_airtable.generate_chapter_data import get_chapter_data
 import json
 
 app = Flask(__name__)
+cache = SimpleCache()
 
 
 @app.route('/')
@@ -12,8 +14,11 @@ def index():
 
 @app.route('/chapter_data.json')
 def chapter_data():
-    # TODO: implement caching
-    data = json.dumps(get_chapter_data())
+    data = cache.get('chapter-data')
+    if data is None:
+        data = json.dumps(get_chapter_data())
+        # Cache for 20 minutes
+        cache.set('chapter-data', data, timeout=20*60)
     return Response(data, status=200, mimetype='application/json')
 
 if __name__ == '__main__':
